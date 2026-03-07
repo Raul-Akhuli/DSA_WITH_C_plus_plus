debce9c7671594502352ecb833a0cb30544b0b9a
Raul <akhuliraul@gmail.com>
2026-03-07 21:59:00 +0530

    add a file zeros moves into last

---
+++ do/ra_05_zeros_at_the_end.cpp
@@ -0,0 +1,40 @@
+// zeros at the end.
+
+# include <iostream>
+# include <vector>
+# include <sstream>
+using namespace std;
+
+int main(){
+	
+	vector <int> vec;
+	string line;
+	getline(cin, line);
+	stringstream ss(line);
+
+	int x;
+	while(ss >> x){
+		vec.push_back(x);
+	}
+	
+	int index = 0;
+	int n = vec.size();
+	for(int i = 0; i < n; i++){
+		
+		if(vec[i] != 0) {
+			
+			vec[index++] = vec[i];
+		}
+	}
+	
+	for(int i = index; i < n; i++){
+	
+		vec[i] = 0;
+	}
+	
+	for(int i = 0; i < n; i++){
+		
+		cout << vec[i] << " ";
+	}
+	return 0;
+}
